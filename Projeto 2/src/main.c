b8c73526a990ef016263af7bb9a366851ee4364f
bgarrido7 <36662037+bgarrido7@users.noreply.github.com>
2018-12-18 19:47:52 +0000

    Update main.c

--- Projeto 2/src/main.c
+++ Projeto 2/src/main.c
@@ -1,15 +1,50 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <netdb.h> 
-#include <sys/types.h>
-#include <netinet/in.h> 
+#include "URL.h"
+#include "FTP.h"
 
-int main(int argc, char *argv[])
-{
+#define SERVER_PORT 21
+#define SERVER_ADDR "192.168.28.96"
+#define STRING_LENGTH 50
 
-		
+int main(int argc, char** argv){
 
-        return 0;
+   	char host[STRING_LENGTH];
+	memset(host, 0, STRING_LENGTH); //poe host a 0
+	url url;
+		url.port = SERVER_PORT;
 
+    struct hostent *h;
+    if (getIp(&url)) {
+		printf("ERROR: Cannot find ip\n");
+		return -1;
+	}
+
+	int	sockfd;
+	struct	sockaddr_in server_addr;
+	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
+	int	bytes;
+	
+	/*server address handling*/
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+    		perror("socket()");
+        	exit(0);
+    	}
+	/*connect to the server*/
+    	if(connect(sockfd, 
+	           (struct sockaddr *)&server_addr, 
+		   sizeof(server_addr)) < 0){
+        	perror("connect()");
+		exit(0);
+	}
+    	/*send a string to the server*/
+	bytes = write(sockfd, buf, strlen(buf));
+	printf("Bytes escritos %d\n", bytes);
+
+	close(sockfd);
+	exit(0);
 }
