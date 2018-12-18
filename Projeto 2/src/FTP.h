f952cb53babe3f198155c272e81e35212fe9bcdf
Diogo <santosdiogo97@gmail.com>
2018-12-18 19:04:39 +0000

    Structure

---
+++ Projeto 2/src/FTP.h
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+
---
+++ Projeto 2/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+
+int main(int argc, char *argv[])
+{
+
+		
+
+        return 0;
+
+}
