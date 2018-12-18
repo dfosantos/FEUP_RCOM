d1de95e2a1860af785031ce100be893ba376951f
bgarrido7 <36662037+bgarrido7@users.noreply.github.com>
2018-12-18 19:50:44 +0000

    Update URL.c

--- Projeto 2/src/URL.c
+++ Projeto 2/src/URL.c
@@ -1,1 +1,17 @@
 #include "URL.h"
+
+int getIp(url* url){
+  struct hostent *h;
+
+    if ((h=gethostbyname(url->host)) == NULL) {  
+        herror("gethostbyname");
+        return 1;
+    }
+
+    printf("Host name  : %s\n", h->h_name);
+    printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+	char* ip = inet_ntoa(*((struct in_addr *) h->h_addr));
+	strcpy(url->ip, ip);
+
+	return 0;
+}
