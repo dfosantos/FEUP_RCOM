effef8c18a6ae915ccd8e081c0c309d8e8ddfc56
bgarrido7 <36662037+bgarrido7@users.noreply.github.com>
2018-12-19 01:14:35 +0000

    Update main.c

--- Projeto 2/src/main.c
+++ Projeto 2/src/main.c
@@ -6,11 +6,11 @@ int main(int argc, char *argv[])
 {
         char* user, password, ip, path, filename, host;
         int port=21;
-        if( processURL( user, password, host, ip, path, filename, argv[1]) != 0)
+        if( processURL( &user, &password, &host, &ip, &path, &filename, argv[1]) != 0)
                 return -1;
 
 
-        if(getIp(host, ip)) {
+        if(getIp(&host, &ip)) {
                 printf("ERROR: Cannot find ip \n");
                 return -1;
         }                    
